48b0d577528c06207f5e8bda7922d46494f19855
Olegsander78 <onkotov@gmail.com>
2021-04-16 14:11:02 +0500

    Реализовал умножение разноразмерных двумерных массивов через методы.

--- Program.cs
+++ Program.cs
@@ -8,70 +8,188 @@ namespace multiMatrix
 {
     class Program
     {
+        /// <summary>
+        /// Создание рандомной матрицы 
+        /// </summary>
+        /// <param name="Col"></param>
+        /// <param name="Str"></param>
+        /// <returns></returns>
+        static int[,] GetRandomMatrix(int Col, int Str)
+        {
+            Random r = new Random();
+            int[,] tempArr = new int[Col, Str];
+            for (int i = 0; i < Col; i++)
+            {
+                for (int j = 0; j < Str; j++)
+                {
+                    tempArr[i, j] = r.Next(100);
+                }
+            }
+            return tempArr;
+        }
+        /// <summary>
+        /// Произведение множителя на матрицу
+        /// </summary>
+        /// <param name="Prod"></param>
+        /// <param name="Arr"></param>
+        static void GetProductMatrix(int Prod, int[,] Arr)
+        {
+            for (int i = 0; i < Arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < Arr.GetLength(1); j++)
+                {
+                    Arr[i, j] *= Prod;
+                }
+            }
+        }
+        /// <summary>
+        /// Метод перемножения разноразмерных матриц , с созданием матрицы размерностью
+        /// по максимальным значениям строк и колонок входных матриц.
+        /// </summary>
+        /// <param name="Arr1"></param>
+        /// <param name="Arr2"></param>
+        /// <returns></returns>
+        static int[,] GetProductMatrixOnMatrix(int[,] Arr1, int[,] Arr2)
+        {
+            int x, y;
+            if (Arr1.GetLength(0) >= Arr2.GetLength(0))
+            {
+                if (Arr1.GetLength(1) >= Arr2.GetLength(1))
+                {
+                    x = Arr1.GetLength(0);
+                    y = Arr1.GetLength(1);
+                }
+                else
+                {
+                    x = Arr1.GetLength(0);
+                    y = Arr2.GetLength(1);
+                }
+            }
+            else
+            {
+                if (Arr1.GetLength(1) >= Arr2.GetLength(1))
+                {
+                    x = Arr2.GetLength(0);
+                    y = Arr1.GetLength(1);
+
+                }
+                else
+                {
+                    x = Arr2.GetLength(0);
+                    y = Arr2.GetLength(1);
+                }
+
+            }
+            int[,] arrTemp = new int[x, y];
+
+            if (Arr1.GetLength(0) >= Arr2.GetLength(0))
+
+            {
+                for (int i = 0; i < Arr1.GetLength(0); i++)
+                {
+                    if (i >= Arr2.GetLength(0)) continue;
+                    if (Arr1.GetLength(1) >= Arr2.GetLength(1))
+
+                    {
+                        for (int j = 0; j < Arr1.GetLength(1); j++)
+                        {
+                            if (j >= Arr2.GetLength(1)) break;
+                            arrTemp[i, j] = Arr1[i, j] * Arr2[i, j];
+                        }
+                    }
+                    else
+                    {
+                        for (int j = 0; j < Arr2.GetLength(1); j++)
+                        {
+                            if (j >= Arr1.GetLength(1)) continue;
+                            arrTemp[i, j] = Arr1[i, j] * Arr2[i, j];
+                        }
+                    }
+                }
+            }
+            else
+            {
+
+                for (int i = 0; i < Arr2.GetLength(0); i++)
+                {
+                    if (i >= Arr1.GetLength(0)) continue;
+                    if (Arr1.GetLength(1) >= Arr2.GetLength(1))
+                    {
+                        for (int j = 0; j < Arr1.GetLength(1); j++)
+                        {
+                            if (j >= Arr2.GetLength(1)) continue;
+                            arrTemp[i, j] = Arr1[i, j] * Arr2[i, j];
+                        }
+                    }
+                    else
+                    {
+                        for (int j = 0; j < Arr2.GetLength(1); j++)
+                        {
+                            if (j >= Arr1.GetLength(1)) continue;
+                            arrTemp[i, j] = Arr1[i, j] * Arr2[i, j];
+                        }
+                    }
+                }
+            }
+
+            return arrTemp;
+        }
+        static void PrintMatrix(int[,] Arr)
+        {
+            for (int i = 0; i < Arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < Arr.GetLength(1); j++)
+                {
+                    Console.Write($"{Arr[i, j],5} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Space()
+        {
+            Console.WriteLine();
+        }
+        static void Delay()
+        {
+            Console.ReadKey();
+        }
+
+
+
+
         static void Main(string[] args)
         {
-            Random rnd = new Random();
-            Console.Write("Введите количесство строк в матрице: ");
+            int[,] matrix1, matrix2, matrix3;
+
+            Console.Write("Введите количество строк в 1 матрице: ");
             int str = int.Parse(Console.ReadLine());
-            Console.Write("Введите количество столбцов в матрице: ");
+            Console.Write("Введите количество столбцов в 1 матрице: ");
             int col = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество строк в 2 матрице: ");
+            int str2 = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество столбцов в 2 матрице: ");
+            int col2 = int.Parse(Console.ReadLine());
             Console.Write("Введите множитель: ");
             int num = int.Parse(Console.ReadLine());
-            int[,] matrix1 = new int[str, col];
-            int[,] matrix2 = new int[str, col];
-            int[,] matrix3 = new int[str, col];
-            Console.WriteLine("\nМатрица 1: \r");
-            Console.WriteLine();
-            for (int i = 0; i < str; i++)
-            {
-                for (int j = 0; j < col; j++)
-                {
-                    matrix1[i, j] = rnd.Next(20);
-                    Console.Write($"| {matrix1[i, j],2} | \t");
+            matrix1 = GetRandomMatrix(str, col);
+            matrix2 = GetRandomMatrix(str2, col2);
+            Console.WriteLine("\nМатрица 1 : ");
+            Space();
+            PrintMatrix(matrix1);
+            Console.WriteLine("\nМатрица 2 : ");
+            Space();
+            PrintMatrix(matrix2);
 
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine("\nМатрица 2: \r");
-            Console.WriteLine();
-            for (int i = 0; i < str; i++)
-            {
-                for (int j = 0; j < col; j++)
-                {                    
-                    matrix2[i, j] = rnd.Next(20);
-                    Console.Write($"| {matrix2[i, j],2} | \t");                    
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine($"\nМатрица 1 умноженная на множитель {num}:");
-            Console.WriteLine();
-            int num2, sumNum;
-
-            for (int i = 0; i < str; i++)
-            {
-                for (int j = 0; j < col; j++)
-                {
-                    num2 = matrix1[i, j];
-                    sumNum = num * num2;
-                    matrix1[i, j] = sumNum;
-                    Console.Write($"|{matrix1[i, j],4} | ");
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine("\nМатрица 3 состоящяя из умножения изменненной матрицы 1 на матрицу 2: ");
-            Console.WriteLine();
-            for (int i = 0; i < str; i++)
-            {
-                for (int j = 0; j < col; j++)
-                {
-                    matrix3[i, j] = matrix1[i, j] * matrix2[i, j];
-                    Console.Write($"|{matrix3[i, j],5} | ");
-                }
-                Console.WriteLine();
-
-            }
-
-            Console.ReadKey();
+            matrix3 = GetProductMatrixOnMatrix(matrix1, matrix2);
+            Console.WriteLine("\nМатрица 3 состоящяя из умножения матрицы 1 на матрицу 2: ");
+            Space();
+            PrintMatrix(matrix3);
+            Space();
+            Console.WriteLine($"\nМатрица 1 умноженная на множитель {num}: ");
+            Space();
+            GetProductMatrix(num, matrix1);
+            PrintMatrix(matrix1);
+            Delay();
         }
     }
 }
